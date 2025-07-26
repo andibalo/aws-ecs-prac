7ebd06c1b84ee0d011ef481ea9017babafa25641
Andi Usman Balo <47916303+andibalo@users.noreply.github.com>
2025-07-26 17:13:50 +0700

    Feat/ci cd workflow (#1)
    * update workflow
    
    * update workflow
    
    * update workflow
    
    * update workflow
    
    * feat(fe): use env for base api url
    
    * update workflow
    
    * update workflow
    
    * update workflow
    
    * update workflow
    
    * update workflow

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const USER_API_BASE_URL = 'http://localhost:8080/users';
+const USER_API_BASE_URL = `${process.env.REACT_APP_API_BASE_URL}/users`;
 
 class UserService {
      async getUsers() {
