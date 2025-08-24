59b57548d80f2627725d51690a631d3e6660e34c
andibalo <andibalo213@gmail.com>
2025-08-24 17:35:02 +0700

    update tf file

--- infra/environments/staging/terraform.tf
+++ infra/environments/staging/terraform.tf
@@ -4,6 +4,8 @@ terraform {
     key    = "state/terraform.tfstate"
     encrypt = true
     region = "ap-southeast-1"
+    shared_config_files      = ["~/.aws/config"]
+    shared_credentials_files = ["~/.aws/credentials"]
     profile = "andib"
   }
 }
